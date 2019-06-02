aad3421a9d96fd475edb07bf8187ea96189c8095
aandro <alona.andro@gmail.com>
2019-06-02 18:39:49 +0300

    + initial project content

---
+++ Assets/Standard Assets/Utility/LevelStatistics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStatistics : MonoBehaviour
+{
+    private int _checkpointCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DoActivateTrigger()
+    {
+        Debug.Log("DoActivateTrigger" + ++_checkpointCount);
+    }
+
+
+}
